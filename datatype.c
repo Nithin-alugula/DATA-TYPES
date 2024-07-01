d59c40f565261a2f198c4d51fc92832857df3690
Nithin-alugula <nithinalugula2@gmail.com>
2024-07-01 21:10:39 +0530

    Create datatype.c

---
+++ datatype.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+		int main(){
+				int a;
+				float b;
+			printf("ENTER THE INT VALUE A:\n");
+			scanf("%d",&a);
+			printf("ENTER THE FLOAT VALUE B:\n");
+			scanf("%f",&b);
+			
+			printf("THE INT VALUE IS:%d\n",a);
+			printf("THE FLOAT VALUE IS:%f\n",b);
+			
+		return 0;
+		
+	}
